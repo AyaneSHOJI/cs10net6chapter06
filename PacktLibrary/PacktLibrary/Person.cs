7bbfc745c613701764e1a3873338c127666b23c4
AyaneSHOJI <shojiayane@gmail.com>
2022-06-07 11:46:17 +0200

    p.220 added classlib

---
+++ PacktLibrary/PacktLibrary/Person.cs
@@ -0,0 +1,17 @@
+﻿using static System.Console;
+
+namespace Packt.Shared;
+
+public class Person : Object
+{
+    // fields
+    public string? Name;
+    public DateTime DateOfBirth;
+    public List<Person> Children = new();
+
+    // methods
+    public void WriteToConsole()
+    {
+        WriteLine($"{ Name } was born on a { DateOfBirth:dddd}.");
+    }
+}
