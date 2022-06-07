2b289afac0ae0005d80c850c7b568cee9260898e
AyaneSHOJI <shojiayane@gmail.com>
2022-06-07 16:39:01 +0200

    p.239 add default implementation

--- PacktLibrary/IPlayable/IPlayable.cs
+++ PacktLibrary/IPlayable/IPlayable.cs
@@ -1,7 +1,13 @@
-﻿namespace Packt.Shared;
+﻿using static System.Console;
+namespace Packt.Shared;
 
 public interface IPlayable
 {
     void Play();
     void Pause();
+    
+    void Stop() // default interface implementation
+    {
+        WriteLine("Default implementation of Stop.");
+    }
 }
