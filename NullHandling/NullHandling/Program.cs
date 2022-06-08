cf8775c1e373dc8c4fe13f3c530284a9a57c5a5b
AyaneSHOJI <shojiayane@gmail.com>
2022-06-08 11:27:44 +0200

    p.249 add non-nullable reference type

--- NullHandling/NullHandling/Program.cs
+++ NullHandling/NullHandling/Program.cs
@@ -1,7 +1,7 @@
 ﻿using static System.Console;
 
 int thisCannotBeNull = 4;
-// thisCannotBeNull = null; // compile ellor 
+// thisCannotBeNull = null; // compile error 
 
 int? thisCouldBeNull = null;
 WriteLine(thisCouldBeNull);
@@ -10,3 +10,22 @@ WriteLine(thisCouldBeNull.GetValueOrDefault());
 thisCouldBeNull = 7;
 WriteLine(thisCouldBeNull);
 WriteLine(thisCouldBeNull.GetValueOrDefault());
+
+// p.249 nullable and non-nullable
+Address address = new();
+address.Building = null;
+address.Street = null;
+address.City = "London";
+address.Region = null;
+
+class Address
+{
+    public string? Building;
+    public string Street;
+    public string City;
+    public string Region;
+}
+
+//public string Street = string.Empty;
+//public string City = string.Empty;
+//public string Region = string.Empty;
