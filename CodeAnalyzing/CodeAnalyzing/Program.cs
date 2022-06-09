2a826310c6448a811000282395886966677a1ef0
AyaneSHOJI <shojiayane@gmail.com>
2022-06-09 13:02:20 +0200

    p.269 fixing the code

--- CodeAnalyzing/CodeAnalyzing/Program.cs
+++ CodeAnalyzing/CodeAnalyzing/Program.cs
@@ -1,9 +1,20 @@
-﻿using System;
+﻿// <copyright file="Program.cs" company="Packt">
+// copyright (c) Packt. All rights reserved.
+// </copyright>
 
 namespace CodeAnalyzing
 {
+    using System;
+
+    /// <summary>
+    ///  The main class for this console app.
+    /// </summary>
     internal class Program
     {
+        /// <summary>
+        /// The main entry point for this console app.
+        /// </summary>
+        /// <param name="args">A string array of arguments passed to the console app.</param>
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
